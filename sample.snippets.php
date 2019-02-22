0af3ef8ad2fe618f14245857f5207a16ca73c38f
macalimosa <calimosa_mark@yahoo.com.ph>
2019-02-22 16:39:16 +0800

    update config

--- sample.snippets.php
+++ sample.snippets.php
@@ -1,35 +1,54 @@
 <?php
 $configs =  [
     'root_data' => 'xerox-widget/data',
-    'hash' => 'conf-5d72e24687a152bffd38ff4631208752bb37a6d3', //config hash from widget
-    'production' => false
+    'hash' => 'your config hash', //config hash from widget
+    'production' => true,
+    'office_printer_id' => 1657, // parent resource
+    'multifunction_printers_id' => 1658, // parent resource
+    'production_printers_id' => 1659, // parent resource
+    'template' => 4,
+    'overwrite' => false
 ];
 
 
 $modx->getService('xerox','xerox',MODX_CORE_PATH.'components/xerox/model/',$configs);
 
-$xerox_products = $modx->xerox->getXeroxProducts();
-$rows = $xerox_products->asArray();
-$update_rows = [];
-$create_rows = [];
-$response = '';
+$xerox_products = $modx->xerox->getXeroxProducts()->asArray();
 
-foreach($rows as $row){
-    $resource = $modx->getObject('modResource', array('pagetitle' => $row['pagetitle']));
-    $method = '';
-    if($resource){
-        $row['id'] = $resource->get('id');
-        $response = $modx->runProcessor('resource/updatenooverride', $row);
-        $update_rows[] = $row;
-        $method = 'update';
-    }else{
-        $response = $modx->runProcessor('resource/create', $row);
-        $create_rows[] = $row;
-        $method = 'create';
+if($modx->xerox->checkFileSize()){
+    $xerox_products = $modx->xerox->getXeroxProducts();
+    $rows = $xerox_products->asArray();
+    $update_rows = [];
+    $create_rows = [];
+    $response = '';
+    foreach($rows as $row){
+        $resource = $modx->getObject('modResource', array('alias' => $row['alias']));
+        $method = '';
+        if($resource){
+            if($configs['overwrite']){
+                $row['id'] = $resource->get('id');
+                $response = $modx->runProcessor('resource/updatenooverride', $row);
+                $update_rows[] = $row;
+                $method = 'update';
+            }
+            
+        }else{
+           
+            $response = $modx->runProcessor('resource/create', $row);
+            $create_rows[] = $row;
+            $method = 'create';
+        }
+        if ($response->isError()) {
+            $modx->log(modX::LOG_LEVEL_ERROR, $method.' error. Processor response: '.$response->getMessage());
+        } else {
+            $modx->log(modX::LOG_LEVEL_ERROR, $row['pagetitle'].' resource '. $method.' successfully');
+        }
     }
-    if ($response->isError()) {
-        $modx->log(modX::LOG_LEVEL_ERROR, $method.' error. Processor response: '.$response->getMessage());
-    } else {
-        $modx->log(modX::LOG_LEVEL_ERROR, 'resource '. $method.' successfully');
-    }
+    //Test
+    // echo '<pre>';
+    // print_r($test);
+    // echo '</pre>';
+ 
+}else{
+    $modx->log(modX::LOG_LEVEL_ERROR, 'No update at' . date('l jS \of F Y h:i:s A'));
 }
